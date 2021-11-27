247a2339de704b38f29a9bf6ad00235ad492cb36
hiyabstefanos <89358080+hiyabstefanos@users.noreply.github.com>
2021-11-27 14:29:22 -0500

    deliverable 1

---
+++ MechaCarChallenge.RScript.R
@@ -0,0 +1,13 @@
+#Deliverable 1-----------------------------------------------------------
+
+#Load dplyr package
+library(dplyr)
+
+#Import and read in the MechaCar_mpg.csv file as a dataframe
+MechaCar <- read.csv(file='MechaCar_mpg.csv',check.names=F,stringsAsFactors = F)
+
+#Perform linear regression
+lm(mpg ~ vehicle_length + vehicle_weight + spoiler_angle + ground_clearance + AWD,data= MechaCar)
+
+#Determine the p-value and the r-squared value for the linear regression model
+summary(lm(mpg ~ vehicle_length + vehicle_weight + spoiler_angle + ground_clearance + AWD,data= MechaCar))
